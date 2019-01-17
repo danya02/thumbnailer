891cd359df5e7cb00f2d9755273dffd16129ac3c
Danya Generalov <danya02@me.com>
2019-01-18 00:30:28 +0300

    Adding an abstraction for a file system.

---
+++ abstract.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import pygame
+from typing import Optional
+
+
+class FileSystemInterface:
+    def __init__(self):
+        """A file system abstraction."""
+        pass
+
+    def get_file_list(self) -> [object]:
+        """
+        Get a list of image file addresses.
+
+        An address is any object that uniquely identifies the image it is referencing.
+        It should be treated by the calling environment as an opaque object.
+        However, it must implement a __repr__() method that recreates that object precisely.
+        """
+        return NotImplemented
+
+    def get_image(self, name, for_thumbnail=False) -> Optional[pygame.Surface]:
+        """
+        Return a Pygame surface with the target image.
+        The image is specified by the address object from get_file_list().
+        If for_thumbnail, the image may have a smaller size than the actual image.
+
+        If an error occurs, this should throw an exception only if not for_thumbnail.
+        If for_thumbnail, this should return None instead.
+        """
+        return None
