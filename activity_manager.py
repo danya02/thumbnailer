c6a21cec38625a24487f8bca1b89c540d33bd4e9
Danya Generalov <danya02@me.com>
2019-01-23 09:33:49 +0300

    Adding license information.

--- activity_manager.py
+++ activity_manager.py
@@ -7,6 +7,22 @@ import pygame
 
 import thumbnail_view
 
+# activity_manager.py - Presentation manager; controls activity switching and drawing.
+# Copyright (C) 2019 Danya Generalov (https://github.com/danya02)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 class ActivityManager:
     def __init__(self):
--- thumbnail_view.py
+++ thumbnail_view.py
@@ -9,6 +9,23 @@ import abstract
 import activity_manager
 import spritesheet_manager
 
+# thumbnail_view.py - Activity to examine the thumbnail list.
+# Copyright (C) 2019 Danya Generalov (https://github.com/danya02)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 
 class ThumbnailView(abstract.GUIActivity):
 
